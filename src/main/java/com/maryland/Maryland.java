315eb2948ce7445fa8505124656a906e6366a62e
Demir Atasoy <datasoy@purdue.edu>
2020-07-25 21:26:32 +0300

    Removed redundant lines of code

--- src/main/java/com/maryland/Maryland.java
+++ src/main/java/com/maryland/Maryland.java
@@ -1,7 +1,5 @@
 package com.maryland;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.function.Supplier;
 import java.util.logging.Logger;
 
@@ -20,13 +18,6 @@ public final class Maryland {
     }
 
     public static void main(String[] args) throws Exception {
-        final Path file = Paths.get("C:\\Users\\Demir Atasoy\\Documents\\Code\\JavaWorkspace\\Maryland\\run\\Plugin-1.0-SNAPSHOT.jar");
-        final Plugin plugin = Plugin.load(file);
-
-        plugin.post("Hello World!");
-
-        System.out.println("".getClass());
-        System.out.println("".getClass().getClass());
 
     }
 }
